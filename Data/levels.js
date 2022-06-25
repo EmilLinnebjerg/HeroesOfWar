d40cb24b67870e8e87a132296e85009438170e1f
EmilLinnebjerg <99876852+EmilLinnebjerg@users.noreply.github.com>
2022-06-25 19:28:22 +0200

    Moving to make levels based on specific temp

---
+++ Data/levels.js
@@ -0,0 +1,23 @@
+class magicShield {
+    forGround = document.createElement("div");
+    middleGroundL = document.createElement("div");
+    middleGroundR = document.createElement("div");
+    backGround = document.createElement("div");
+    baseL = document.createElement("div");
+    baseR = document.createElement("div");
+    
+    constructor(){
+        this.forGround.style.width = 850 + "px";
+        this.forGround.style.height = 500 + "px";
+        
+        this.middleGroundL.style.width = 850 + "px";
+        this.middleGroundL.style.height = 500 + "px";
+
+        this.middleGroundR.style.width = 850 + "px";
+        this.middleGroundR.style.height = 500 + "px";
+
+        this.backGround.style.width = 850 + "px";
+        this.backGround.style.height = 500 + "px";
+    }
+
+}
