4b219ca5b62ff950922f5882db74049865c4dd3f
EmilLinnebjerg <99876852+EmilLinnebjerg@users.noreply.github.com>
2022-07-02 13:39:49 +0200

    removed old code

--- Data/singleplayer.js
+++ Data/singleplayer.js
@@ -7,16 +7,7 @@ singlePlayerMap.style.top = 0 + "px";
 singlePlayerMap.style.left = 0 + "px";
 
 function loadSinglePlayer(){
-    clearInterval(menuAnim);
-    document.getElementById("bm");
-    bm.loop = true;
-    bm.play();
-    document.getElementById('menuDiv').style.visibility = 'hidden';
-    document.getElementById('mnuStart').style.visibility = 'hidden';
-    document.getElementById('gameTitle').style.visibility = 'hidden';
-    document.getElementById('background').style.visibility = 'hidden';
-    document.getElementById('foreground').style.visibility = 'hidden';
-    document.getElementById('instructions').style.visibility = 'hidden';
+
 
     alert("here");
 
